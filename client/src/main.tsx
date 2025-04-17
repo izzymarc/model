1f673bf991bb8574b05497a7618b4cd5d1d908ff
izzymarc <izzymarc@gmail.com>
2025-04-17 11:38:14 +0100

    Fix remixicon import by using CDN in index.css instead of direct package import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import '@/lib/i18n';
-import 'remixicon/fonts/remixicon.css';
 import ErrorBoundary from './components/common/ErrorBoundary';
 
 // Theme initialization
