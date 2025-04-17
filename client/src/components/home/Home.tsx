58f6150369ee9e4beccfcefcf195859858b7734c
izzymarc <izzymarc@gmail.com>
2025-04-17 11:09:41 +0100

    Fix Home.tsx component to ensure it has one parent element

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import Instagram from './Instagram';
 import Blog from './Blog';
+import Contact from './Contact';
 
+const Home = () => {
+  return (
+    <>
       <Instagram />
       <Blog />
-      <Contact /> 
+      <Contact />
+    </>
+  );
+};
+
+export default Home; 
